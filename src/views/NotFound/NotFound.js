0e52d705790337b7c08e1a8dc9d0b495e4952c2b
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-03 10:01:34 -0700

    Tweak 404 page.

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -35,17 +35,11 @@ const NotFound = () => {
         >
           <div className={classes.content}>
             <Typography variant="h1">
-              404: The page you are looking for isn’t here
+              Not Found
             </Typography>
             <Typography variant="subtitle2">
-              You either tried some shady route or you came here by mistake.
-              Whichever it is, try using the navigation
+              We're sorry but this page was not found.
             </Typography>
-            <img
-              alt="Under development"
-              className={classes.image}
-              src="/images/undraw_page_not_found_su7k.svg"
-            />
           </div>
         </Grid>
       </Grid>
