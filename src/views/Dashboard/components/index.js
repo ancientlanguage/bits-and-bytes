d31539c2e2ede026fbd8aa1707758335eddc9a95
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-06 06:11:26 -0700

    Remove reference to MoreData.

--- src/views/Dashboard/components/index.js
+++ src/views/Dashboard/components/index.js
@@ -3,4 +3,3 @@ export { default as Data } from './Data';
 export { default as CardThree } from './CardThree';
 export { default as CardFour } from './CardFour';
 export { default as CardTwo } from './CardTwo';
-export { default as MoreData } from './MoreData';
