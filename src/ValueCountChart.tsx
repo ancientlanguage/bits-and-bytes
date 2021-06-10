543c4d8d9a70e2dc744aa33effc5521ab9581806
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-10 14:54:01 -0700

    Omit form control for selects.

--- src/ValueCountChart.tsx
+++ src/ValueCountChart.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Select, FormControl, MenuItem, Container } from '@material-ui/core';
+import { Select, MenuItem, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { Bar } from 'react-chartjs-2';
 
@@ -79,37 +79,35 @@ export const ValueCountChart = (props: { infos: Info[] }) => {
 
   return (
     <div className={classes.chartContainer}>
-      <FormControl>
-        <Container>
-          <Select
-            id="bar-direction"
-            value={barDirection}
-            onChange={handleChangeBarDirection}
-            className={classes.barOptionSelect}
-          >
-            <MenuItem value={'vertical'}>Vertical</MenuItem>
-            <MenuItem value={'horizontal'}>Horizontal</MenuItem>
-          </Select>
-          <Select
-            id="bar-scale"
-            value={scale}
-            onChange={handleChangeScale}
-            className={classes.barOptionSelect}
-          >
-            <MenuItem value={'linear'}>Linear</MenuItem>
-            <MenuItem value={'logarithmic'}>Logarithmic</MenuItem>
-          </Select>
-          <Select
-            id="bar-values"
-            value={showValues}
-            onChange={handleChangeShowValues}
-            className={classes.barOptionSelect}
-          >
-            <MenuItem value={'all'}>All Values</MenuItem>
-            <MenuItem value={'occurring'}>Occurring Values</MenuItem>
-          </Select>
-        </Container>
-      </FormControl>
+      <Container>
+        <Select
+          id="bar-direction"
+          value={barDirection}
+          onChange={handleChangeBarDirection}
+          className={classes.barOptionSelect}
+        >
+          <MenuItem value={'vertical'}>Vertical</MenuItem>
+          <MenuItem value={'horizontal'}>Horizontal</MenuItem>
+        </Select>
+        <Select
+          id="bar-scale"
+          value={scale}
+          onChange={handleChangeScale}
+          className={classes.barOptionSelect}
+        >
+          <MenuItem value={'linear'}>Linear</MenuItem>
+          <MenuItem value={'logarithmic'}>Logarithmic</MenuItem>
+        </Select>
+        <Select
+          id="bar-values"
+          value={showValues}
+          onChange={handleChangeShowValues}
+          className={classes.barOptionSelect}
+        >
+          <MenuItem value={'all'}>All Values</MenuItem>
+          <MenuItem value={'occurring'}>Occurring Values</MenuItem>
+        </Select>
+      </Container>
       <Bar type="bar" data={data} options={options} />
     </div>
   );
