44fd1e1cda65a996841528ec202aae1112b5a5f0
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-10 07:26:15 -0700

    Start over and load bytes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,62 @@
-import { Component } from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { useState } from 'react';
+import theme from './theme';
+import { Button, CssBaseline, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
-import theme from './theme';
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import './assets/css/index.css';
-import Routes from './Routes';
+function sendRequest(setBytes: (bytes: Uint8Array) => void) {
+  const request = new XMLHttpRequest();
+  const url = 'https://raw.githubusercontent.com/scott-fleischman/ucd/Unicode-13.0/UnicodeData.txt';
+  const isAsync = true;
+  request.open('GET', url, isAsync);
+  request.responseType = 'arraybuffer';
+  const startTime = performance.now();
 
-const browserHistory = createBrowserHistory();
+  request.onload = onLoadRequest(startTime, setBytes, request);
 
-export default class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Router history={browserHistory}>
-          <Routes />
-        </Router>
-      </ThemeProvider>
-    );
-  }
+  request.send(null);
 }
+
+const onLoadRequest =
+  (startTime: number, setBytes: (bytes: Uint8Array) => void, request: XMLHttpRequest) =>
+    (_: ProgressEvent<EventTarget>) => {
+      const onloadTime = performance.now();
+      const loadTime = onloadTime - startTime;
+      const url = request.responseURL;
+      console.log({ message: 'onload url', url, loadTime });
+
+      const arrayBuffer: ArrayBuffer = request.response;
+      if (!arrayBuffer) {
+        console.log({ error: 'Unable to load url' });
+        return;
+      }
+      console.log({ byteLength: arrayBuffer.byteLength })
+
+      setBytes(new Uint8Array(arrayBuffer));
+    };
+
+const Loader = () => {
+  const [getBytes, setBytes] = useState<Uint8Array>(new Uint8Array());
+  const onClick = () => {
+    sendRequest(setBytes);
+  };
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={onClick}>
+        Bits and Bytes
+      </Button>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {getBytes.length + ' bytes'} 
+      </Typography>
+    </div>
+  );
+};
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Loader />
+  </ThemeProvider>
+);
+
+export default App;
