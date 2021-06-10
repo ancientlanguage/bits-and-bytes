55614aaf5bfd4683c77181719e121e7ccd792ac2
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-10 14:05:38 -0700

    Add options for scale and occurring values.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import theme from './theme';
-import { Button, CssBaseline, Typography } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import { makeStyles } from '@material-ui/styles';
+import { Button, CssBaseline, Select, FormControl, MenuItem, Typography, Container } from '@material-ui/core';
+import { ThemeProvider, makeStyles } from '@material-ui/styles';
 import { Bar } from 'react-chartjs-2';
 
 function sendRequest(setBytes: (bytes: Uint8Array) => void) {
@@ -41,35 +40,75 @@ const useStyles = makeStyles(() => ({
     height: 400,
     position: 'relative'
   },
+  barOptionSelect: {
+    marginRight: '12px'
+  }
 }));
+
+type Info = { label: string; count: number; };
+type Scale = 'linear' | 'logarithmic';
+type ShowValues = 'all' | 'occurring';
 
 const Loader = () => {
   const [bytes, setBytes] = useState<Uint8Array>(new Uint8Array());
+  const [scale, setScale] = useState<Scale>('linear');
+  const [showValues, setShowValues] = useState<ShowValues>('all');
   const onClick = () => {
     sendRequest(setBytes);
   };
   const classes = useStyles();
 
-  const labels: string[] = new Array(256);
-  const counts: number[] = new Array(256);
+  const infos: Info[] = new Array(256);
   for (let index = 0; index < 256; index++) {
-    counts[index] = 0;
-    labels[index] = index.toString();
+    infos[index] = {
+      label: index.toString(),
+      count: 0,
+    };
   }
   const byteValues = bytes.values();
   for (const byte of byteValues) {
-    const existingCount = counts[byte];
-    counts[byte] = existingCount + 1;
+    const existing = infos[byte];
+    existing.count = existing.count + 1;
   }
-  
+  let infosToShow: Info[];
+  switch (showValues) {
+    case 'all':
+      infosToShow = infos;
+      break;
+    case 'occurring':
+      infosToShow = infos.filter((info: Info) => info.count > 0);
+      break;
+  }
+
   const data = {
-    labels: labels,
+    labels: infosToShow.map((info: Info) => info.label),
     datasets: [{
-      label: 'Byte count',
-      data: counts,
+      label: 'Count',
+      data: infosToShow.map((info: Info) => info.count),
     }]
   };
-  
+
+  const options = {
+    indexAxis: 'y',
+    scales: {
+      x: {
+        display: true,
+        type: scale,
+      },
+      y: {
+        display: true,
+      }
+    }
+  };
+
+  const handleChangeScale = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setScale(event.target.value as Scale);
+  };
+
+  const handleChangeShowValues = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setShowValues(event.target.value as ShowValues);
+  };
+
   return (
     <div>
       <Button variant="contained" color="primary" onClick={onClick}>
@@ -80,7 +119,29 @@ const Loader = () => {
       </Typography>
 
       <div className={classes.chartContainer}>
-        <Bar type="bar" data={data} />
+        <FormControl>
+          <Container>
+            <Select
+              id="bar-scale"
+              value={scale}
+              onChange={handleChangeScale}
+              className={classes.barOptionSelect}
+            >
+              <MenuItem value={'linear'}>Linear</MenuItem>
+              <MenuItem value={'logarithmic'}>Logarithmic</MenuItem>
+            </Select>
+            <Select
+              id="bar-values"
+              value={showValues}
+              onChange={handleChangeShowValues}
+              className={classes.barOptionSelect}
+            >
+              <MenuItem value={'all'}>All Values</MenuItem>
+              <MenuItem value={'occurring'}>Occurring Values</MenuItem>
+            </Select>
+          </Container>
+        </FormControl>
+        <Bar type="bar" data={data} options={options} />
       </div>
     </div>
   );
