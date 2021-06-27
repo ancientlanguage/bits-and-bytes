3b79fc2969ce2c5fa464d7763e9fc723259fb3ca
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-27 07:57:01 -0700

    Use menu.

--- src/View/StructureTagView.tsx
+++ src/View/StructureTagView.tsx
@@ -1,12 +1,12 @@
-import { Grid, TextField, Tooltip } from '@material-ui/core';
+import { Button, Grid, Menu, MenuItem, TextField, Typography } from '@material-ui/core';
 import CategoryIcon from '@material-ui/icons/Category';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ListIcon from '@material-ui/icons/List';
 import PieChartIcon from '@material-ui/icons/PieChart';
 import StopIcon from '@material-ui/icons/Stop';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import React from 'react';
+import React, { useState } from 'react';
 import { StructureTag } from 'State/Structure';
+import * as UUID from 'uuid';
 
 const Metadata = (props: { structureTag: StructureTag; }) => {
   return (
@@ -14,13 +14,53 @@ const Metadata = (props: { structureTag: StructureTag; }) => {
   );
 }
 
+const getTagText = (tag: StructureTag) => {
+  switch (tag) {
+    case 'atom': return 'Atom';
+    case 'or': return 'Or';
+    case 'and': return 'And';
+    case 'list': return 'List';
+  }
+}
+
+const getTagIcon = (tag: StructureTag) => {
+  switch (tag) {
+    case 'atom': return <StopIcon />;
+    case 'or': return <PieChartIcon />;
+    case 'and': return <CategoryIcon />;
+    case 'list': return <ListIcon />;
+  }
+}
+
+const makeTagDescription = (tag: StructureTag) => {
+  return (<>
+    {getTagIcon(tag)} < Typography > {getTagText(tag)}</Typography >
+  </>);
+};
+
 const StructureTagView = (props: { structureTag: StructureTag; setStructureTag: (structureTag: StructureTag) => void; }) => {
   const { structureTag, setStructureTag } = props;
+  const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+  const [menuId] = useState<string>(() => UUID.v4());
 
-  const handleStructureTag = (event: React.MouseEvent<HTMLElement>, newStructureTag: string | null) => {
-    if (newStructureTag !== null) {
-      setStructureTag(newStructureTag as StructureTag);
-    }
+  const handleStructureTag = (event: React.MouseEvent<HTMLElement>, tag: StructureTag) => {
+    setStructureTag(tag);
+    setAnchorElement(null);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElement(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
+
+  const menuItems: StructureTag[] = ['atom', 'or', 'and', 'list'];
+  const makeMenuItem = (tag: StructureTag) => {
+    return (
+      <MenuItem key={tag} onClick={(e) => handleStructureTag(e, tag)}>{makeTagDescription(tag)} </MenuItem>
+    );
   };
 
   return (
@@ -32,33 +72,18 @@ const StructureTagView = (props: { structureTag: StructureTag; setStructureTag: 
       spacing={1}
     >
       <Grid item>
-        <ToggleButtonGroup
-          value={structureTag}
-          exclusive
-          onChange={handleStructureTag}
-          aria-label="Structure Type"
+        <Button color="inherit" aria-controls={menuId} aria-haspopup="true" onClick={handleClick} variant="outlined">
+          {makeTagDescription(structureTag)} <ExpandMoreIcon />
+        </Button>
+        <Menu
+          id={menuId}
+          anchorEl={anchorElement}
+          keepMounted
+          open={Boolean(anchorElement)}
+          onClose={handleClose}
         >
-          <ToggleButton value="atom" aria-label="Atom">
-            <Tooltip title="Atom">
-              <StopIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value="or" aria-label="Or">
-            <Tooltip title="Or">
-              <PieChartIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value="and" aria-label="And">
-            <Tooltip title="And">
-              <CategoryIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value="list" aria-label="List">
-            <Tooltip title="List">
-              <ListIcon />
-            </Tooltip>
-          </ToggleButton>
-        </ToggleButtonGroup>
+          {menuItems.map((i) => makeMenuItem(i))}
+        </Menu>
       </Grid>
       <Grid item>
         <Metadata structureTag={structureTag} />
