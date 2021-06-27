0d2ca84d586313fa128e9d7fc52619366feb98e6
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-27 07:29:28 -0700

    Use top-level Grid.

--- src/View/Layout.tsx
+++ src/View/Layout.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, ButtonBase, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, ButtonBase, Grid, Toolbar, Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -74,60 +74,67 @@ const Layout = (props: { appState: AppState; dispatch: React.Dispatch<AppAction>
   });
 
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Box marginRight={1}>
-            <ButtonBase onClick={() => goToPath('/')}>
-              <LogoIcon />
-            </ButtonBase>
-          </Box>
-          <Box display="flex" flexGrow={1}>
-            <Button color="inherit" aria-controls="nav-menu" aria-haspopup="true" onClick={handleClick}>
-              <Typography>{currentPageName}</Typography> <ExpandMoreIcon />
-            </Button>
-            <Menu
-              id="nav-menu"
-              anchorEl={anchorElement}
-              keepMounted
-              open={Boolean(anchorElement)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={() => goToPath('/')}>Home</MenuItem>
-              <MenuItem onClick={() => goToPath('/data-sources')}>Data Sources</MenuItem>
-              <MenuItem onClick={() => goToPath('/structures')}>Structures</MenuItem>
-              <MenuItem onClick={() => goToPath('/mappings')}>Mappings</MenuItem>
-              <MenuItem onClick={() => goToPath('/chains')}>Chains</MenuItem>
-              <MenuItem onClick={() => goToPath('/settings')}>Settings</MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </AppBar>
+    <Grid
+      container
+      direction="column"
+    >
+      <Grid item>
+        <AppBar position="static">
+          <Toolbar>
+            <Box marginRight={1}>
+              <ButtonBase onClick={() => goToPath('/')}>
+                <LogoIcon />
+              </ButtonBase>
+            </Box>
+            <Box display="flex" flexGrow={1}>
+              <Button color="inherit" aria-controls="nav-menu" aria-haspopup="true" onClick={handleClick}>
+                <Typography>{currentPageName}</Typography> <ExpandMoreIcon />
+              </Button>
+              <Menu
+                id="nav-menu"
+                anchorEl={anchorElement}
+                keepMounted
+                open={Boolean(anchorElement)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => goToPath('/')}>Home</MenuItem>
+                <MenuItem onClick={() => goToPath('/data-sources')}>Data Sources</MenuItem>
+                <MenuItem onClick={() => goToPath('/structures')}>Structures</MenuItem>
+                <MenuItem onClick={() => goToPath('/mappings')}>Mappings</MenuItem>
+                <MenuItem onClick={() => goToPath('/chains')}>Chains</MenuItem>
+                <MenuItem onClick={() => goToPath('/settings')}>Settings</MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Grid>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/data-sources">
-          <DataSourceList />
-        </Route>
-        <Route exact path="/structures">
-          <Structures />
-        </Route>
-        <Route exact path="/mappings">
-          <Mappings />
-        </Route>
-        <Route exact path="/chains">
-          <Chains />
-        </Route>
-        <Route exact path="/settings">
-          <Settings settings={props.appState.settings} dispatch={settingsDispatch} />
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
+      <Grid item>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/data-sources">
+            <DataSourceList />
+          </Route>
+          <Route exact path="/structures">
+            <Structures />
+          </Route>
+          <Route exact path="/mappings">
+            <Mappings />
+          </Route>
+          <Route exact path="/chains">
+            <Chains />
+          </Route>
+          <Route exact path="/settings">
+            <Settings settings={props.appState.settings} dispatch={settingsDispatch} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Grid>
+    </Grid>
   );
 };
 
