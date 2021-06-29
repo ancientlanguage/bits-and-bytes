2998da7e9afb60473b5707b2f9ec69ead4b428f8
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2021-06-29 06:36:56 -0700

    Use tree view.

--- src/View/StructureView.tsx
+++ src/View/StructureView.tsx
@@ -1,4 +1,8 @@
 import { Box } from '@material-ui/core';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TreeItem from '@material-ui/lab/TreeItem';
+import TreeView from '@material-ui/lab/TreeView';
 import React from 'react';
 import { StructureTag } from 'State/Structure';
 import StructureTagView from './StructureTagView';
@@ -8,20 +12,38 @@ const StructureView = () => {
   const [structureTag2, setStructureTag2] = React.useState<StructureTag>('atom');
   const [structureTag3, setStructureTag3] = React.useState<StructureTag>('atom');
 
+  const ignoreLabelClick = (event: React.MouseEvent<Element>) => {
+    event.preventDefault();
+  }
+
   return (
     <Box display="flex" flexDirection="column">
-      <StructureTagView
-        structureTag={structureTag}
-        setStructureTag={setStructureTag}
-      />
-      <StructureTagView
-        structureTag={structureTag2}
-        setStructureTag={setStructureTag2}
-      />
-      <StructureTagView
-        structureTag={structureTag3}
-        setStructureTag={setStructureTag3}
-      />
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId="1" onLabelClick={ignoreLabelClick} label={
+          <StructureTagView
+            structureTag={structureTag}
+            setStructureTag={setStructureTag}
+          />
+        }>
+          <TreeItem nodeId="2" onLabelClick={ignoreLabelClick} label={
+            <StructureTagView
+              structureTag={structureTag2}
+              setStructureTag={setStructureTag2}
+            />
+          }
+          />
+          <TreeItem nodeId="3" onLabelClick={ignoreLabelClick} label={
+            <StructureTagView
+              structureTag={structureTag3}
+              setStructureTag={setStructureTag3}
+            />
+          }
+          />
+        </TreeItem>
+      </TreeView>
     </Box>
   );
 }
